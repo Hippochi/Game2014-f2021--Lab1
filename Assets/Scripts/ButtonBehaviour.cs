8940ff0146862ba4eb925e218202ce996e76fda7
Alex Dine <alexdine1996@gmail.com>
2021-09-16 21:56:42 -0400

    updated with button functionality

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+   public void OnStartButtonPress()
+    {
+        Debug.Log("Start Button Pressed");
+
+    }
+}
